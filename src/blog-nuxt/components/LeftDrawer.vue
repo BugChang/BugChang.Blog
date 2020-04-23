607cb7df5cc3719346b0d4db058640fe62ce0bdf
BugChang <81069681@qq.com>
2020-04-23 23:35:06 +0800

    test

---
+++ src/blog-nuxt/components/LeftDrawer.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-navigation-drawer v-model="drawer" class="secondary" app>
+    <v-list rounded>
+      <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: {
+    drawer: Boolean,
+  },
+  data() {
+    return {
+      items: [
+        {
+          title: '首页',
+          to: '/',
+          icon: 'mdi-home',
+        },
+        {
+          title: '分类',
+          to: '/category',
+          icon: 'mdi-shape',
+        },
+        {
+          title: '标签',
+          to: '/tags',
+          icon: 'mdi-tag',
+        },
+        {
+          title: '关于',
+          to: '/about',
+          icon: 'mdi-information',
+        },
+      ],
+    }
+  },
+}
+</script>
--- src/blog-nuxt/layouts/default.vue
+++ src/blog-nuxt/layouts/default.vue
@@ -1,29 +1,37 @@
+<style>
+.act {
+  border-bottom: pink 2px solid;
+}
+</style>
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
       <v-toolbar-title>BugChang's Blog</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text to="/" active-class="act">
+          <v-icon>mdi-home</v-icon>首 页</v-btn
+        >
+        <v-menu offset-y active-class="act">
+          <template v-slot:activator="{ on }">
+            <v-btn text dark v-on="on">
+              分类<v-icon>mdi-menu-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item to="/1">
+              <v-list-item-title>分类1</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn text to="/tags" active-class="act">标签</v-btn>
+        <v-btn text to="/about" active-class="act">关于</v-btn>
+        <v-btn text to="/contact" active-class="act">联系</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-    <v-navigation-drawer class="primary lighten-1" permanent app dark>
-      <v-list rounded>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <template v-slot:append>
-        <div class="pa-2">
-          <v-btn block>Logout</v-btn>
-        </div>
-      </template>
-    </v-navigation-drawer>
-    <v-content>
+    <!-- <LeftDrawer :drawer="drawer"></LeftDrawer> -->
+    <v-content class="mt-4">
       <nuxt />
     </v-content>
     <v-footer app>
@@ -36,14 +44,12 @@
 </template>
 
 <script>
+// import LeftDrawer from '@/components/LeftDrawer'
 export default {
+  // components: { LeftDrawer },
   data() {
     return {
-      items: [
-        { title: 'Dashboard', icon: 'mdi-plus' },
-        { title: 'Account', icon: 'mdi-account' },
-        { title: 'Admin', icon: 'mdi-gavel' },
-      ],
+      drawer: false,
     }
   },
 }
