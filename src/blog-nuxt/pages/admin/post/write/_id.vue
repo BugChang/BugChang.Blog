f0888a5866da5f9ac8cc62dda6acec199c516a9f
changji <81069681@qq.com>
2020-04-28 17:39:35 +0800

    写文章页面细化

--- src/blog-nuxt/pages/admin/post/write/_id.vue
+++ src/blog-nuxt/pages/admin/post/write/_id.vue
@@ -1,20 +1,120 @@
 <template>
   <v-container fluid class="write-container">
     <v-form class="d-flex flex-column write-form">
-      <v-text-field label="标题"></v-text-field>
+      <v-row justify="center">
+        <v-col>
+          <v-text-field v-model="post.title" label="标题"></v-text-field>
+          <v-file-input
+            v-show="false"
+            id="coverImageInput"
+            label="上传封面图"
+            @change="uploadCoverImg"
+          >
+          </v-file-input>
+        </v-col>
+      </v-row>
       <div ref="mavonEditorDiv" class="mavonEditor">
         <no-ssr>
           <mavon-editor
             ref="mavonEditor"
-            v-model="handbook"
+            v-model="post.content"
             :toolbars="markdownOption"
             :style="`height:${editorHeight}px`"
           />
         </no-ssr>
       </div>
-      <div class="mt-2">
-        <v-btn color="primary">发布</v-btn>
-        <v-btn color="primary">保存草稿</v-btn>
+      <div>
+        <v-row align="center" justify="start">
+          <v-switch
+            v-model="post.isPublish"
+            class="mx-2"
+            label="发布"
+          ></v-switch>
+          <v-switch
+            v-model="post.isPublish"
+            class="mx-2"
+            label="置顶"
+          ></v-switch>
+          <div style="width: 160px;">
+            <v-select
+              v-model="post.categoryId"
+              :items="categories"
+              item-text="name"
+              item-value="id"
+              label="分类"
+              class="mx-2"
+            ></v-select>
+          </div>
+          <v-chip
+            v-for="tag in post.tags"
+            :key="tag"
+            class="mx-2"
+            close
+            @click:close="removePostTag(tag)"
+            >{{ tag }}</v-chip
+          >
+          <div style="width: 120px;">
+            <v-text-field
+              v-model="postTagInput.text"
+              class="mx-2"
+              label="添加标签"
+              :error="postTagInput.error"
+              :error-messages="postTagInput.errorMsg"
+              @keypress.enter="addPostTag"
+            ></v-text-field>
+          </div>
+          <v-dialog v-model="coverImgDialog" width="40%" max-width="850">
+            <template v-slot:activator="{ on }">
+              <v-btn class="mx-2" color="primary" v-on="on">
+                <v-icon left>mdi-camera</v-icon>
+                封面图片</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title>封面图片管理</v-card-title>
+              <v-img :src="post.coverImg" max-width="100%">
+                <v-btn-toggle v-if="post.coverImg" class="cover-options" dark>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" @click="selectCoverImg">
+                        <v-icon>mdi-camera</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>更换</span>
+                  </v-tooltip>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" @click="clearCoverImg">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>删除</span>
+                  </v-tooltip>
+                </v-btn-toggle>
+
+                <v-btn
+                  v-else
+                  text
+                  block
+                  height="80"
+                  color="primary"
+                  @click="selectCoverImg"
+                >
+                  <v-icon left>mdi-camera</v-icon>
+                  上传封面
+                </v-btn>
+              </v-img>
+              <v-card-actions>
+                <v-btn block text @click="coverImgDialog = false">关闭</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <v-spacer></v-spacer>
+          <v-btn color="primary" class="mx-2" @click="save">
+            <v-icon left>mdi-content-save</v-icon>保存
+          </v-btn>
+        </v-row>
       </div>
     </v-form>
   </v-container>
@@ -24,12 +124,30 @@ export default {
   layout: 'admin',
   data() {
     return {
+      coverImgDialog: false,
       editorHeight: 0,
+      categories: [],
+      post: {
+        id: 0,
+        title: '',
+        content: '',
+        categoryId: null,
+        coverImg: '',
+        isPublish: false,
+        tags: [],
+      },
+      postTagInput: {
+        text: '',
+        error: false,
+        errorMsg: '',
+      },
       markdownOption: {
         bold: true, // 粗体
       },
-      handbook: '#### how to use mavonEditor in nuxt.js',
     }
+  },
+  created() {
+    this.getCategories()
   },
   mounted() {
     this.calcEditorHeight()
@@ -39,8 +157,45 @@ export default {
   },
   methods: {
     calcEditorHeight() {
-      console.log(1)
-      this.editorHeight = this.$refs.mavonEditorDiv.clientHeight
+      this.editorHeight = this.$refs.mavonEditorDiv.clientHeight - 20
+    },
+    async getCategories() {
+      const data = await this.$axios.$get('/categories')
+      this.categories = data
+    },
+    save() {
+      if (this.post.id > 0) {
+        // post
+      } else {
+        // put
+      }
+    },
+    addPostTag() {
+      const tagText = this.postTagInput.text
+      const postTags = this.post.tags
+      if (postTags.length >= 5) return
+      if (tagText)
+        if (!postTags.includes(tagText)) {
+          postTags.push(tagText)
+        }
+      this.clearPostTagInput()
+    },
+    removePostTag(tag) {
+      const index = this.post.tags.indexOf(tag)
+      this.post.tags.splice(index, 1)
+    },
+    clearPostTagInput() {
+      this.postTagInput.text = ''
+    },
+    selectCoverImg() {
+      document.getElementById('coverImageInput').click()
+    },
+    uploadCoverImg(file) {
+      const windowURL = window.URL || window.webkitURL
+      this.post.coverImg = windowURL.createObjectURL(file)
+    },
+    clearCoverImg() {
+      this.post.coverImg = ''
     },
   },
 }
@@ -57,6 +212,11 @@ export default {
 .write-container,
 .write-form {
   height: 100%;
+}
+.cover-options {
+  position: absolute;
+  right: 0;
+  bottom: 0;
 }
 </style>
 <style>
